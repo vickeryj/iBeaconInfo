402fd925adf19bb0b13fa039fe4234b3ca853945
Joshua Vickery <josh@vickeryj.com>
2021-06-24 16:22:37 -0400

    Initial Commit

---
+++ iBeaconInfo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iBeaconInfo
+//
+//  Created by Joshua Vickery on 6/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
